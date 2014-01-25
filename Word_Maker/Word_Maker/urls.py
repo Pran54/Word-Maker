17ac9e5854acbfb0cd56d7fc00c59aa42f888c31
Pran54 <pran828@gmail.com>
2014-01-24 20:38:38 -0500

    Update urls.py

--- Word_Maker/Word_Maker/urls.py
+++ Word_Maker/Word_Maker/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'Generate_Words.views.home', name='home'),
+    url(r'^test$', 'Generate_Words.views.home', name='test'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
